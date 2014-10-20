47d3ba1ccafec925056a60ae3466980c029d70f0
nlitwino <litwinowicz.4@buckeyemail.osu.edu>
2014-10-19 23:13:19 -0400

    Open Gallery Button Added
    Added a button to open the gallery and select an image. The image does
    not persist however as of now.

--- app/src/main/java/com/osu/cse5236/pocketshop/OpenPictureActivity.java
+++ app/src/main/java/com/osu/cse5236/pocketshop/OpenPictureActivity.java
@@ -1,20 +1,30 @@
 package com.osu.cse5236.pocketshop;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class OpenPictureActivity extends Activity {
 
     private final String TAG = ((Object)this).getClass().getSimpleName();
+    private static final int SELECT_PICTURE = 1;
+    private String selectedImagePath;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_picture);
+        initializeGetPictureFromGallery();
+
         Log.e(TAG, "++ In onCreate() ++");
     }
 
@@ -65,5 +75,21 @@ public class OpenPictureActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeGetPictureFromGallery() {
+        Button fromGallery = (Button) findViewById(R.id.center);
+        fromGallery.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // in onCreate or any event where your want the user to
+                // select a file
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+            }
+        });
     }
 }
